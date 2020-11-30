92b2155d52567c73b247c744394e3b28cdd09569
hammedb197 <30192139+hammedb197@users.noreply.github.com>
2020-12-01 00:14:42 +0100

    Update infer.py

--- infer.py
+++ infer.py
@@ -26,17 +26,21 @@ def load_image_into_numpy_array(path):
 
 
 image_np = load_image_into_numpy_array(image_path)
+print(image_np)
 input_tensor = tf.convert_to_tensor(image_np)
+
 input_tensor = input_tensor[tf.newaxis, ...]
 detections = detect_fn(input_tensor)
 num_detections = int(detections.pop('num_detections'))
 detections = {key:value[0,:num_detections].numpy() for key,value in detections.items()}
+print(detection)
 detections['num_detections'] = num_detections
 detections['detection_classes'] = detections['detection_classes'].astype(np.int64)
 image_np_with_detections = image_np.copy()
 viz_utils.visualize_boxes_and_labels_on_image_array( image_np_with_detections, detections['detection_boxes'], detections['detection_classes'],
                                                     detections['detection_scores'], category_index,
                                                     use_normalized_coordinates=True, max_boxes_to_draw=100, min_score_thresh=.5,  agnostic_mode=False)
+print(image_np_with_detections)
 cv2.imwrite('final.jpg', image_np_with_detections)
 # %matplotlib inline
 plt.figure()
